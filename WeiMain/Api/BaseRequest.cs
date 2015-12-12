e8e8463571e9470d851426706d5e5ab52be0d7ee
Jarlene <xwl198937@hotmail.com>
2015-12-12 16:49:32 +0800

    add http request on loacal

---
+++ WeiMain/Api/BaseRequest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WeiMain.Api
+{
+    public abstract class BaseRequest
+    {
+        protected const string BASE_URL = @"http://tingapi.ting.baidu.com/v1/restserver/ting?method={0}&format=json&from={1}&version={2}";
+        protected const string FROM = @"wp10";
+        protected const string VERSION = @"1.0.0.0";
+
+        public string ContentErrorCode { get; set; }
+        public bool RequestSucceeded { get; set; }
+        public HttpStatusCode HttpStatusCode { get; set; }
+
+        public BaseRequest()
+        {
+            ContentErrorCode = ErrorCode.OK;
+            RequestSucceeded = false;
+            HttpStatusCode = HttpStatusCode.OK;
+        }
+        
+
+        public abstract string GetMethodName();
+
+        protected abstract string GetQueryContent();
+
+        protected abstract Task ParseAsync(string json);
+
+
+        public bool IsRequestAllOK()
+        {
+            return RequestSucceeded && HttpStatusCode == HttpStatusCode.OK && ContentErrorCode == ErrorCode.OK;
+        }
+
+        private string GetUrl()
+        {
+            string url = string.Format(BASE_URL, GetMethodName(), FROM, VERSION);
+            string content = GetQueryContent();
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                url += ("&" + content);
+            }
+            return url;
+        }
+
+        private void ResponseCallback(IAsyncResult result)
+        {
+            if(result == null)
+            {
+                RequestSucceeded = false;
+                return;
+            }
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)result.AsyncState;
+                WebResponse response = request.EndGetResponse(result);
+                using (Stream stream = response.GetResponseStream())
+                using(StreamReader reader = new StreamReader(stream))
+                {
+                    string responseContent = reader.ReadToEnd();
+                    if (string.IsNullOrEmpty(responseContent))
+                    {
+                        RequestSucceeded = false;
+                        return;
+                    } else
+                    {
+                        RequestSucceeded = true;
+                        ParseAsync(responseContent);
+                    }
+
+                    
+                }
+            }
+            catch( Exception e)
+            {
+                RequestSucceeded = false;
+            } 
+
+        }
+
+        public void AsyncRequest()
+        {
+            string url = GetUrl();
+            var request = HttpWebRequest.Create(url);
+            request.ContentType = "application/json";
+            request.Method = "GET";
+            request.BeginGetResponse(ResponseCallback, request);
+        }
+    }
+}
---
+++ WeiMain/Api/ErrorCode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeiMain.Api
+{
+    public static class ErrorCode
+    {
+        public static readonly string OK = "22000";
+        public static readonly string Failed = "22001";
+        public static readonly string ActionToOften = "22011";  //操作过于频繁
+        public static readonly string EncNEQ = "22013";         //加密内容和明文不相等
+        public static readonly string DecFailed = "22014";      //解密失败
+
+        public static readonly string FavoriteIllegal = "22321";            //非法的收藏类型
+        public static readonly string FavoriteDuplicated = "22322";         //收藏重复
+        public static readonly string FavoriteSongCountExceeded = "22331";  //收藏歌曲达到上限
+        public static readonly string FavoriteAlbumCountExceeded = "22332"; //收藏歌曲达到上限
+        public static readonly string FavoriteDiyCountExceeded = "22333";   //收藏歌曲达到上限
+
+        public static readonly string UserAvatarUpdateFailed = "22451";     //用户头像更新失败
+        public static readonly string UserHaveNotLoggedIn = "22452";        //用户未登录
+        public static readonly string UserHaveNotActivated = "22453";       //用户尚未激活
+        public static readonly string UserHaveBeenDeleted = "22454";        //用户已经被删除
+        public static readonly string UserGetPassportInfoFailed = "22455";  //查询用户passport信息失败
+        public static readonly string UserAddBaseInfoFailed = "22456";      //添加用户基本信息失败
+        public static readonly string UserTingUidAssignedFailed = "22457";  //用户分配ting uid失败
+        public static readonly string UserIsActivated = "22458";            //用户已激活
+        public static readonly string UserNickNameEmpty = "22459";          //用户昵称为空
+        public static readonly string UserSignTooLong = "22460";            //用户签名过长
+        public static readonly string UserVersionTooLow = "22461";          //用户版本过低
+        public static readonly string UserDownloadLimit = "22462";          //用户下载受限
+        public static readonly string UserIPLimit = "22463";                //用户IP受限无法下载
+        public static readonly string UserVersionLow = "22464";				//用户版本建议升级
+    }
+}
